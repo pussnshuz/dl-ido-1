86d2b0a4dbe6c77ede16ed9671e9a9b87960ba1b
eugene5208 <135844101+eugene5208@users.noreply.github.com>
2023-10-26 11:21:56 +0300

    Update task2.1(1).py

--- Лабораторная 2/task2.1.py
+++ Лабораторная 2/task2.1.py
@@ -3,13 +3,11 @@ salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
 months = 0
-Bankrupt = True
 # TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
-while Bankrupt:
+while True:
+    if (money_capital + salary) < spend:
+        break
     months += 1
     money_capital += salary - spend
-    if (money_capital + salary) < spend:
-        Bankrupt = not Bankrupt
-        break
     spend += spend * increase
 print("Количество месяцев, которое можно протянуть без долгов:", months)
