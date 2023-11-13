69fa922bb88f0c910fc9316a7ccaabc4a83b0b02
eugene5208 <135844101+eugene5208@users.noreply.github.com>
2023-11-13 18:29:01 +0300

    Update task4.2.py

--- lab4/task4.2.py
+++ lab4/task4.2.py
@@ -11,10 +11,10 @@ def task() -> None:
         lines = [line for line in csv.DictReader(f)]
 
     ...  # TODO Сериализовать в файл с отступами равными 4
-    file = open(OUTPUT_FILENAME, 'w', encoding='utf8')
-    json_lines = json.dumps(lines, indent=4)
-    file.write(json_lines)
-    file.close()
+    with open(OUTPUT_FILENAME, 'w', encoding='utf8') as file:
+        json_lines = json.dumps(lines, indent=4)
+        file.write(json_lines)
+
 
 
 if __name__ == '__main__':
